c2cf1b18e69e28137abe7c2fc82aa676b5b8aa1d
David Bergeron <david.f.bergeron@gmail.com>
2019-07-25 19:15:30 -0600

    Added an index prop for map id, so I can have multiple maps on one page

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 var $S = require('scriptjs');
 
 class SimpleGoogleMaps extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -12,51 +12,57 @@ class SimpleGoogleMaps extends Component {
     };
   }
 
-  componentDidMount(){
-    window.google ? this.setState({
-      mapLoaded:true
-    }) :
-    $S(`https://maps.googleapis.com/maps/api/js?key=${this.props.apiKey}`, () => {
-      this.setState({
-        mapLoaded:true
-      });
-    });
-  }
-
-  render(){
-    if(this.state.mapLoaded){
-      var map = new window.google.maps.Map(document.getElementById('map'), {
-        zoom: this.props.zoom,
-        center: {
-          lat: this.props.center.lat,
-          lng: this.props.center.lng
-        }
-      });
-      if(this.props.markers){
-        if(Array.isArray(this.props.markers)){
-          for(let i of this.props.markers){
-            new google.maps.Marker({
-              position: i,
-              map: map,
+  componentDidMount() {
+    window.google
+      ? this.setState({
+          mapLoaded: true
+        })
+      : $S(
+          `https://maps.googleapis.com/maps/api/js?key=${this.props.apiKey}`,
+          () => {
+            this.setState({
+              mapLoaded: true
             });
           }
-        }else{
+        );
+  }
+
+  render() {
+    if (this.state.mapLoaded) {
+      var map = new window.google.maps.Map(
+        document.getElementById(`map-${this.props.index}`),
+        {
+          zoom: this.props.zoom,
+          center: {
+            lat: this.props.center.lat,
+            lng: this.props.center.lng
+          }
+        }
+      );
+      if (this.props.markers) {
+        if (Array.isArray(this.props.markers)) {
+          for (let i of this.props.markers) {
+            new google.maps.Marker({
+              position: i,
+              map: map
+            });
+          }
+        } else {
           new google.maps.Marker({
             position: this.props.markers,
-            map: map,
+            map: map
           });
         }
       }
     }
-    return (
-      <div id='map' style={this.props.style}></div>
-    );
+    return <div id={`map-${this.props.index}`} style={this.props.style} />;
   }
 }
 
 export default SimpleGoogleMaps;
 
 SimpleGoogleMaps.propTypes = {
+  index: PropTypes.number.isRequired,
   zoom: PropTypes.number.isRequired,
   apiKey: PropTypes.string.isRequired,
   center: PropTypes.objectOf(PropTypes.number.isRequired),
@@ -65,4 +71,4 @@ SimpleGoogleMaps.propTypes = {
     PropTypes.objectOf(PropTypes.number),
     PropTypes.arrayOf(PropTypes.object)
   ])
-}
+};
